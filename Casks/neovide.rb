e859771efeeaa512d89ae1396274f5bf1c63d178
Tobias Walle <tobias.walle@maibornwolff.de>
2021-12-23 15:27:47 +0100

    fix syntax

--- Casks/neovide.rb
+++ Casks/neovide.rb
@@ -2,7 +2,7 @@ cask "neovide" do
   version "0.7.0"
   sha256 :no_check
 
-  url https://github.com/Kethku/neovide/releases/download/#{version}/Neovide.app.zip"
+  url "https://github.com/Kethku/neovide/releases/download/#{version}/Neovide.app.zip"
   name "neovide"
   desc "No Nonsense Neovim Client in Rust"
   homepage "https://github.com/Kethku/neovide"
