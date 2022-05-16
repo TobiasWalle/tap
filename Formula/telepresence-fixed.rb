114ac755eb0b963e28a42485f8bfbc07ff3e7f57
Tobias Walle <tobias.walle@maibornwolff.de>
2022-05-16 10:41:28 +0200

    update tp

--- Formula/telepresence-fixed.rb
+++ Formula/telepresence-fixed.rb
@@ -3,7 +3,7 @@
 class TelepresenceFixed < Formula
   desc "Local dev environment attached to a remote Kubernetes cluster"
   homepage "https://telepresence.io"
-  url "https://app.getambassador.io/download/tel2/darwin/amd64/2.4.9/telepresence"
+  url "https://app.getambassador.io/download/tel2/darwin/amd64/2.5.8/telepresence"
 
   # macfuse is a cask and formula can't depend on casks, so we can't actually
   # do this. This is probably fine since you don't _need_ macfuse to run
